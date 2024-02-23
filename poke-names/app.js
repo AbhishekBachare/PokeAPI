88f30113b5c1654acd389400da9b546a45fd7c94
AbhishekBachare <abhishekbachare@gmail.com>
2024-02-23 18:16:07 +0530

    4th 'pokelist' branch commit - Updated ReadMe.md in main directory & /poke-names.

--- poke-names/app.js
+++ poke-names/app.js
@@ -1,3 +1,4 @@
+// app.js
 
 const express = require('express');
 const axios = require('axios');
@@ -9,55 +10,29 @@ app.set('view engine', 'ejs');
 
 const ITEMS_PER_PAGE = 20;
 
-const fetchPokemonDetails = async (url) => {
-  try {
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching Pokémon details:', error.message);
-    throw error;
-  }
-};
-
-const fetchPokemonPage = async (offset) => {
+const fetchPokemonList = async (offset) => {
   try {
     const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${ITEMS_PER_PAGE}`);
-    const pokemonData = await Promise.all(response.data.results.map(async (pokemon) => {
-      const details = await fetchPokemonDetails(pokemon.url);
-      return {
-        name: pokemon.name,
-        details,
-      };
-    }));
-
-    return pokemonData;
+    return response.data;
   } catch (error) {
-    console.error('Error fetching Pokémon data:', error.message);
+    console.error('Error fetching Pokémon list:', error.message);
     throw error;
   }
 };
 
 app.get('/', async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-
   try {
+    const page = parseInt(req.query.page) || 1;
     const offset = (page - 1) * ITEMS_PER_PAGE;
-    const pokemonList = await fetchPokemonPage(offset);
-    res.render('index', { pokemonList, currentPage: page });
+    const pokemonList = await fetchPokemonList(offset);
+
+    res.render('index', {
+      pokemonList: pokemonList.results,
+      currentPage: page,
+      totalPages: Math.ceil(pokemonList.count / ITEMS_PER_PAGE),
+    });
   } catch (error) {
     res.status(500).send('Internal Server Error');
-  }
-});
-
-app.get('/pokemon/:name', async (req, res) => {
-  const pokemonName = req.params.name;
-
-  try {
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    const pokemonDetails = response.data;
-    res.render('pokemon', { pokemonDetails });
-  } catch (error) {
-    res.status(404).send('Pokémon not found');
   }
 });
 
